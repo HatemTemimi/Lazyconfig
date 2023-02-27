781084423bcccd52ec1b337842c6aa4958234cba
katana66 <hatem.temimi@estale.fr>
2023-02-27 22:04:58 +0100

    update plugs

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -17,7 +17,7 @@ return {
         return math.floor(vim.o.lines * 0.75)
       end,
       max_width = function()
-        return math.floor(vim.o.columns * 0.25)
+        return math.floor(vim.o.columns * 0.30)
       end,
     },
     init = function()
