16ea7341d5ecce114130d9565854007b01f51957
Hatem Temimi <hatem.temimi@estale.fr>
2023-09-20 12:07:46 +0100

    fix: highlife board

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -21,7 +21,7 @@ return {
         dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
         dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
         dashboard.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
+        dashboard.button("l", "鈴" .. " Highlife", ":Lazy<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
       for _, button in ipairs(dashboard.section.buttons.val) do
