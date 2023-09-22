cfe15619709170a4157527194175a1cb2a6e92ff
Hatem Temimi <hatem.altemimi@gmail.com>
2023-09-22 21:59:29 +0100

    fix: border curved

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -5,12 +5,7 @@ return {
     open_mapping = [[<c-t>]],
     direction= 'float',
     float_opts = {
-    -- The border key is *almost* the same as 'nvim_open_win'
-    -- see :h nvim_open_win for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    border = 'curved'
-    -- like `size`, width and height can be a number or function which is passed the current terminal
+      border = 'curved'
   },
   },
 }
